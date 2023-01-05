1238a55d4815abab67b00ea27a62c8c61eceda32
sogyeonghak <sogyeonghak@github.com>
2023-01-05 09:02:38 +0900

    chore : loginpageUI

---
+++ src/main/java/kt/carpool/utils/AES256.java
@@ -0,0 +1,52 @@
+package kt.carpool.utils;
+
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AES256 {
+
+    public static String alg = "AES/CBC/PKCS5Padding";
+    private final String key = "01234567890123456789012345678901";
+    private final String iv = key.substring(0, 16); // 16byte
+
+    public String encrypt(String text) throws Exception {
+        Cipher cipher = Cipher.getInstance(alg);
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
+
+        byte[] encrypted = cipher.doFinal(text.getBytes("UTF-8"));
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    public String decrypt(String cipherText) throws Exception {
+        Cipher cipher = Cipher.getInstance(alg);
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParamSpec);
+
+        byte[] decodedBytes = Base64.getDecoder().decode(cipherText);
+        byte[] decrypted = cipher.doFinal(decodedBytes);
+        return new String(decrypted, "UTF-8");
+    }
+
+}
+
+// 예제
+
+//public class Main {
+//
+//    public static void main(String[] args) throws Exception {
+//
+//        AES256 aes256 = new AES256();
+//        String text = "!! Hello World !!";
+//        String cipherText = aes256.encrypt(text);
+//        System.out.println(text);
+//        System.out.println(cipherText);
+//        System.out.println(aes256.decrypt(cipherText));
+//
+//    }
+//
+//}
